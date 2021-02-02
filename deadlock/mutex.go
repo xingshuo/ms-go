5c5ea9f44cf03d1285678c11f4b5e9263d619048
lakefu <lakefu@tencent.com>
2021-02-02 20:55:41 +0800

    add some debug info

--- deadlock/mutex.go
+++ deadlock/mutex.go
@@ -34,10 +34,12 @@ type RWMutex struct {
 	L sync.RWMutex
 	Ref int64
 	WaitRef int64
+	lastGid int64
 }
 
 func (m *RWMutex) Lock() {
 	if Opts.Disable {
+		panic("lock disable")
 		m.L.Lock()
 		return
 	}
@@ -45,6 +47,7 @@ func (m *RWMutex) Lock() {
 	detector.addWaiter(m, gid)
 	atomic.AddInt64(&m.WaitRef, 1)
 	m.L.Lock()
+	m.lastGid = gid
 	atomic.AddInt64(&m.Ref, 1)
 	atomic.AddInt64(&m.WaitRef, -1)
 	detector.addLocker(m, gid)
@@ -52,16 +55,19 @@ func (m *RWMutex) Lock() {
 
 func (m *RWMutex) Unlock() {
 	if Opts.Disable {
+		panic("unlock disable")
 		m.L.Unlock()
 		return
 	}
 	atomic.AddInt64(&m.Ref, -1)
+	m.lastGid = 0
 	m.L.Unlock()
 	detector.delLocker(m)
 }
 
 func (m *RWMutex) RLock() {
 	if Opts.Disable {
+		panic("rlock disable")
 		m.L.RLock()
 		return
 	}
@@ -69,6 +75,7 @@ func (m *RWMutex) RLock() {
 	detector.addWaiter(m, gid)
 	atomic.AddInt64(&m.WaitRef, 1)
 	m.L.RLock()
+	m.lastGid = gid
 	atomic.AddInt64(&m.Ref, 1)
 	atomic.AddInt64(&m.WaitRef, -1)
 	detector.addLocker(m, gid)
@@ -76,10 +83,12 @@ func (m *RWMutex) RLock() {
 
 func (m *RWMutex) RUnlock() {
 	if Opts.Disable {
+		panic("runlock disable")
 		m.L.RUnlock()
 		return
 	}
 	atomic.AddInt64(&m.Ref, -1)
+	m.lastGid = 0
 	m.L.RUnlock()
 	detector.delLocker(m)
 }
